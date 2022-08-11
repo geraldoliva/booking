53cf0e4032fa87048efe23f8dddb24b44f709a1f
Gerald Oliva <geraldsamoliva@gmail.com>
2022-08-11 17:02:32 -0400

    Styled Navbar component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,12 @@
+import Navbar from "../../components/navbar/Navbar";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
